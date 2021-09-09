5f69708a2f86959889534bc1ebafd9973a3d7220
mariammaher550 <36730799+mariammaher550@users.noreply.github.com>
2021-09-09 18:59:39 +0300

    Update ex01.c

--- ex01.c
+++ ex01.c
@@ -2,10 +2,16 @@
 #include  <unistd.h>
 #include <sys/types.h>
 #include <stdlib.h>
-
+#include <sys/wait.h>
 int main (){
+    int n = 5;
     for(int i = 0; i < 10; i++) {
         int x = fork();
-        printf(x != 0 ? "Hello from parent %d\n" : "Hello from child %d\n", getpid());
+      //  wait(NULL);
+        if (x != 0) {
+            printf("Hello from parent [%d - %d]\n", getpid(), n);
+        } else {
+            printf("Hello from child [%d - %d]\n", getpid(), n);
+        }
     }
 }
