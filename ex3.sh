fc77e0875c350309e4ba23494abde453f91acf63
mariammaher550 <36730799+mariammaher550@users.noreply.github.com>
2021-10-26 23:48:00 +0300

    Create ex3.sh

---
+++ ex3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+touch _ex3.txt
+chmod a-x _ex3.txt
+ls -l _ex3.txt > ex3.txt
+chmod u=rwx _ex3.txt
+chmod o=rwx _ex3.txt
+chmod g=0 _ex3.txt
+ls -l _ex3.txt >> ex3.txt
+chmod g=u _ex3.txt
+ls -l _ex3.txt >> ex3.txt
+
+
+
