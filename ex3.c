9a36bc8db3281fc7e95249666ff8ad048d6d6584
mariammaher550 <36730799+mariammaher550@users.noreply.github.com>
2021-11-09 23:46:44 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -11,41 +11,26 @@
 int main(){
     char *dev = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
     int fd = open(dev, 0,O_RDONLY);
+    int keys[100] = {};
     if(fd < 0) {
         return EXIT_FAILURE;
     }
     else {
 
             struct input_event ev1;
-            int seq[3] = {};
             int cnt = 0;
             while(true) {
                 ssize_t b = read(fd, &ev1, sizeof(ev1));
-                //released
-                if((ev1.value == 0 || ev1.value == 1) && ev1.type == EV_KEY )
-                {
-                    seq[cnt] = ev1.code;
-                    cnt++;
-                }
-                if(cnt == 2)
-                {
-                    if(seq[0] == 80 && seq[1] == 69)
-                    {
-                        printf("I PASSED\n");
-                    }
-                }
-                else if (cnt == 3)
-                {
-                    if(seq[0] == 67 && seq[1] == 65 && seq[2] == 80)
-                    {
-                        cnt = 0;
-                        memset(seq, 0, sizeof (seq));
-                        printf("I WANT COFFEE\n");
+                if(ev1.type == EV_KEY) {
+                    keys[ev1.code] = ev1.value;
+                        if (keys[25] = 2 && keys[18] == 2 || keys[25] == 1 && keys[18] == 1 ) {
+                            printf("I PASSED\n");
+                        if (keys[46]==2&&keys[30]==2&&keys[25]==2||keys[46]==1&&keys[30]==1&& keys[25]==1) {
+                            printf("I WANT COFFEE\n");
+                        }
                     }
                 }
             }
     }
     fflush(stdout);
 }
-
-
